bd94fa93fe52d4ffd140a07b5457a0875aba47e2
pablo-maff <pablo.maff.076@gmail.com>
2022-06-06 12:20:42 +0200

    Ex 13.3

---
+++ cli.js
@@ -0,0 +1,26 @@
+const { POSTGRES_URI, PORT } = require('./utils/config')
+const { Sequelize, QueryTypes } = require('sequelize')
+
+const sequelize = new Sequelize(POSTGRES_URI, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+})
+
+const main = async () => {
+  try {
+    await sequelize.authenticate()
+    const blogs = await sequelize.query('SELECT * FROM blogs', {
+      type: QueryTypes.SELECT,
+    })
+    console.log(blogs)
+    sequelize.close()
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
+
+main()
